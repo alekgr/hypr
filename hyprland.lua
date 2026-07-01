44f05fd5e5222c2104ddfa6323209efe875b8ac9
alekgr <alek.grigorian@gmail.com>
2026-07-01 13:12:28 -0500

    removed misc comments

--- hyprland.lua
+++ hyprland.lua
@@ -202,18 +202,6 @@ hl.config({
     },
 })
 
-----------------
-----  MISC  ----
-----------------
-
--- hl.config({
---         misc = {
---         	force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
---         	disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
--- 			focus_on_activate = false,
---     	},
--- })
-
 require('misc')
 require('input')
 
