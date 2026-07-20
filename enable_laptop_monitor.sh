e7b5f16eb54025fe45c62822babfd09399ebfafd
alekgr <alek.grigorian@gmail.com>
2026-07-20 17:52:49 -0500

    rm scripts that was used in old styple config

--- enable_laptop_monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ln -sfn ~/.config/hypr/custom/rocket_hyprland_laptop.conf hyprland.conf
-
-pkill waybar
-
-ln -sfn ~/.config/waybar/rocket_laptop_monitor_config ~/.config/waybar/config
-
-hyprctl dispatch exec waybar
