f22f3fee66b77cac486991c15aed12fbbfb4fc6b
alekgr <alek.grigorian@gmail.com>
2026-07-26 05:59:08 -0500

    fix double loading background

--- misc.lua
+++ misc.lua
@@ -5,7 +5,7 @@
 hl.config({
         misc = {
         	force_default_wallpaper = -1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        	disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
+        	disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
 		focus_on_activate = false,
 		initial_workspace_tracking = 0 
     	},
